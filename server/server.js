686df53cb966b471d5053075f3c456eb611cbda3
David Hybner <dah1998@yahoo.com>
2022-11-30 00:53:57 -0500

    install packages and create mutation on client side
    tried to run sever but I am running into errors. created mutation on client side and updated server and auth.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const app = express();
 const PORT = process.env.PORT || 3001;
 //set up apollo sever
@@ -11,8 +13,8 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-//setup middleware
-server.applyMiddleware({ app });
+// //setup middleware
+// server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,8 +28,15 @@ if (process.env.NODE_ENV === 'production') {
  app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname, '../client/build/index.html'));
  });
-
+ const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+ };
+
+ startApolloServer(typeDefs, resolvers);
