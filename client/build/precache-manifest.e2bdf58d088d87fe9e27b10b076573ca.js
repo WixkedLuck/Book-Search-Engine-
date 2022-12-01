064c30aa0fb8f8a1a6e1b4a8f37b2e80f733e223
David Hybner <dah1998@yahoo.com>
2022-12-01 00:39:21 -0500

    take away favicon
    deleted favicon to try to fix heroklu

---
+++ client/build/precache-manifest.e2bdf58d088d87fe9e27b10b076573ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a932a69ef47c5492b81f8ab1446b9c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9f3586dd899578d007e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "eb79f2936e12c1d7a205",
+    "url": "/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "c9f3586dd899578d007e",
+    "url": "/static/js/2.2697e26a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2697e26a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb79f2936e12c1d7a205",
+    "url": "/static/js/main.cfba0bf2.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
